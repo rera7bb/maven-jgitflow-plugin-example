2138666e7b4ba49b07334a7bf100dc2833d67b6a
Ryoichi Era <rera@voszero.com>
2017-11-08 04:57:16 +0900

    0.2 develop 1

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,12 +2,13 @@ package com.example;
 
 /**
  * Hello world!
- *
+ * 
  */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "0.2 implement on develop branch 1" );
     }
 }
